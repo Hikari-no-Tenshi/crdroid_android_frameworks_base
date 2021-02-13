2eeaaa77b99686dca05d86d4e4d65e8e3ed790e5
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-02-13 13:21:05 +0200

    FODCircleView: Update position on pulsing

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -122,6 +122,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
     private boolean mIsBouncer;
     private boolean mIsDreaming;
+    private boolean mIsPulsing;
     private boolean mIsKeyguard;
     private boolean mIsCircleShowing;
     private boolean mIsShowing;
@@ -176,6 +177,16 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
                 mBurnInProtectionTimer.schedule(new BurnInProtectionTask(), 0, 60 * 1000);
             } else if (mBurnInProtectionTimer != null) {
                 mBurnInProtectionTimer.cancel();
+                updatePosition();
+            }
+        }
+
+        @Override
+        public void onPulsing(boolean pulsing) {
+            super.onPulsing(pulsing);
+            mIsPulsing = pulsing;
+
+            if (mIsPulsing) {
                 updatePosition();
             }
         }
@@ -714,7 +725,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
             mFODAnimation.updateParams(mParams.y);
         }
 
-        if (mIsDreaming && !mIsCircleShowing) {
+        if (mIsDreaming && !mIsPulsing && !mIsCircleShowing) {
             mParams.y += mDreamingOffsetY;
         }
 
