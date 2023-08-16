c9879cf25d5bcfdc86a2c2240287ae0407131c5b
aswin7469 <aswinas@pixysos.com>
2023-08-16 22:32:44 +0530

    PixelPropUtils: Stop spoofing some google apps
    * non pixels doesnt need to spoof asi to enable most features
    * fixes now playing in pixel devices
      [asi is checked with device codename to load sountrigger models for now playing]
    
    * motionsense is limited to p19 devices only
    
    Change-Id: Icf651e95548f524ff8f1dd3556a8d4f6197745f4
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -76,17 +76,20 @@ public class PixelPropsUtils {
             "com.google.android.MTCL83",
             "com.google.android.UltraCVM",
             "com.google.android.apps.cameralite",
+            "com.google.android.apps.motionsense.bridge",
             "com.google.android.apps.pixelmigrate",
             "com.google.android.apps.recorder",
             "com.google.android.apps.restore",
             "com.google.android.apps.wearables.maestro.companion",
             "com.google.android.apps.youtube.kids",
             "com.google.android.apps.youtube.music",
+            "com.google.android.as",
             "com.google.android.dialer",
             "com.google.android.euicc",
             "com.google.android.setupwizard",
             "com.google.android.youtube",
-            "com.google.ar.core"
+            "com.google.ar.core",
+            "com.google.oslo"
     };
 
     private static final Map<String, Object> propsToChangeROG1;
