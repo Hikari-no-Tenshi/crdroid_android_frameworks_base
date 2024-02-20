49018ef7e70a018ceaab9739071feb34e5d1122a
minaripenguin <minaripenguin@users.noreply.github.com>
2024-02-20 12:33:24 +0800

    PixelPropUtils: Fix bootloader spoofing
    * aside from sign-in failing due to empty hardware identifier,
      earlier patch also introduced another regression which resulted to breakage of sign-up,
      fix it by using propimitation hooks `onEngineGetCertificateChain` and blacklisting setupwizard.
    
    test: do factory reset, do device setup, google sign-in works and setup completed without errors
    
    Change-Id: Ia81ed7c9c07d42ff975f059fda8e45b10e2a457d
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -124,6 +124,7 @@ public class PixelPropsUtils {
 
     private static volatile boolean sIsGms = false;
     private static volatile boolean sIsFinsky = false;
+    private static volatile boolean sIsExcluded = false;
 
     static {
         propsToKeep = new HashMap<>();
@@ -199,6 +200,7 @@ public class PixelPropsUtils {
 
             if (Arrays.asList(packagesToKeep).contains(packageName) ||
                     packageName.startsWith("com.google.android.GoogleCamera")) {
+                sIsExcluded = true;
                 return;
             }
 
@@ -340,7 +342,7 @@ public class PixelPropsUtils {
 
     public static void onEngineGetCertificateChain() {
         // Check stack for SafetyNet or Play Integrity
-        if (isCallerSafetyNet() || sIsFinsky) {
+        if ((isCallerSafetyNet() || sIsFinsky) && !sIsExcluded) {
             Log.i(TAG, "Blocked key attestation sIsGms=" + sIsGms + " sIsFinsky=" + sIsFinsky);
             throw new UnsupportedOperationException();
         }
